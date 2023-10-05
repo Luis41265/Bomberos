9937fc0a64b94028e8d8930d21aedbf4631e3c99
Jose Carlos Bran <jbranadev@gmail.com>
2023-10-04 22:47:36 -0600

    Creación de Token Interceptor Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import {HttpClientModule} from "@angular/common/http";
-
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import {ApirestService} from "./services/apirest.service";
+import {TokenInterceptorServiceService} from "./services/token-interceptor-service.service";
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptorServiceService,
+      multi: true
+    },
     ApirestService
 
   ],
---
+++ src/app/services/token-interceptor-service.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
+import {ApirestService} from "./apirest.service";
+import { Router } from '@angular/router';
+import { throwError } from "rxjs";
+import { catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenInterceptorServiceService implements HttpInterceptor {
+
+  constructor(private apirest:ApirestService,
+              private router:Router) { }
+
+
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    // Get the auth token from the service.
+    const authToken = this.apirest.getAuthorizationToken();
+
+    // Clone the request and replace the original headers with
+    // cloned headers, updated with the authorization.
+    const authReq = req.clone({
+      headers: req.headers.set('Authorization', authToken)
+    });
+
+    // send cloned request with header to the next handler.
+    return next.handle(authReq).pipe(
+      catchError(err => {
+          // onError
+          console.log(err);
+          if (err instanceof HttpErrorResponse) {
+            console.log(err.status);
+            console.log(err.statusText);
+            if (err.status === 401) {
+              this.apirest.presentAlert();
+              this.router.navigate(['/login']);
+            }
+          }
+          return throwError(err);
+        }
+      )
+    );
+  }
+
+
+}
